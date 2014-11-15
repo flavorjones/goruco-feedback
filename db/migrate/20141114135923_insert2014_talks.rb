15e6807073b406e960a5692efbff5ff094ea573b
Mike Dalessio <mike.dalessio@gmail.com>
2014-11-15 10:29:02 -0500

    talks

--- db/migrate/20141114135923_insert2014_talks.rb
+++ db/migrate/20141114135923_insert2014_talks.rb
@@ -2,9 +2,17 @@ class Insert2014Talks < ActiveRecord::Migration
   def change
     talks = [
       ["Aaron Quint",	"The Future of Ruby Performance Testing"],
-      ["Foo",	"Foo bar"],
-      ["Bazz",	"Bazz Wut"],
-      ["Quux",	"Quux Bunny"],
+      ["Lisa Larson-Kelly",	"Rail-time Communication with WebRTC"],
+      ["Mike Bernstein",	"Know Your Types .."],
+      ["Andrew Turley",	"What We Can Learn From COBOL"],
+      ["Andrew Troutt",	"Teaching Kids to Code on Raspberry Pi"],
+      ["Solomon Kahn",	"BI Tooling With Rails"],
+      ["Chris Hunt",	"Secrets of a World Memory Champion"],
+      ["Luke Melia",	"Growing a Tech Community"],
+      ["Nathan Artz",	"Developing and Testing Third Party Javascript Widgets"],
+      ["S. John & J. Brennan",	"Designin a Better Programmer Community"],
+      ["Michael May",	"Edge Caching for Dynamic Rails Apps"],
+      ["James Golick",	"How to Debug Anything"],
     ]
 
     Talk.transaction do

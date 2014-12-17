e58266419812476352ccb75f03756e8754f43250
Mike Dalessio <mike.dalessio@gmail.com>
2014-12-17 14:30:00 -0500

    More information about talks.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,24 @@
 class Talk < ActiveRecord::Base
-  has_and_belongs_to_many :responses
+  has_many :responses, through: :talk_responses
+  has_many :talk_responses
+
+  def scores
+    @scores ||= talk_responses.collect(&:score).collect(&:to_i)
+  end
+
+  def min
+    scores.min
+  end
+
+  def avg
+    scores.inject(0) { |sum, score| sum += score }.to_f / scores.length
+  end
+
+  def max
+    scores.max
+  end
+
+  def comments
+    talk_responses.collect(&:comment).select(&:present?)
+  end
 end
